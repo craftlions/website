5b89d2a01e7d0e9d05fc77db1750f285c11f3574
Alexander Niebuhr <alexander@nbhr.io>
2024-05-21 08:00:55 +0200

    update FAQ

--- apps/site/astro.config.ts
+++ apps/site/astro.config.ts
@@ -1,7 +1,7 @@
-import { defineConfig } from 'astro/config';
 import cloudflare from '@astrojs/cloudflare';
-import icon from 'astro-icon';
 import react from '@astrojs/react';
+import icon from 'astro-icon';
+import { defineConfig } from 'astro/config';
 
 // https://astro.build/config
 export default defineConfig({
