d092c84e30ccb05152d6f638adfdcc520b2c39a3
Alexander Niebuhr <45965090+alexanderniebuhr@users.noreply.github.com>
2026-05-09 19:16:14 +0200

    Update database schema for api key
    Signed-off-by: Alexander Niebuhr <45965090+alexanderniebuhr@users.noreply.github.com>

--- src/lib/schema.ts
+++ src/lib/schema.ts
@@ -183,6 +183,31 @@ export const rateLimit = t.pgTable("rate_limit", {
 	lastRequest: t.bigint("last_request", { mode: "number" }).notNull(),
 });
 
+export const apikey = t.pgTable("apikey", {
+	id: t.text("id").primaryKey().unique(),
+	configId: t.text("config_id").notNull(),
+	name: t.text("name"),
+	start: t.text("start"),
+	prefix: t.text("prefix"),
+	key: t.text("key").notNull(),
+	referenceId: t.text("reference_id").notNull(),
+	refillInterval: t.integer("refill_interval"),
+	refillAmount: t.integer("refill_amount"),
+	lastRefillAt: t.timestamp("last_refill_at", { withTimezone: true }),
+	enabled: t.boolean("enabled"),
+	rateLimitEnabled: t.boolean("rate_limit_enabled"),
+	rateLimitTimeWindow: t.integer("rate_limit_time_window"),
+	rateLimitMax: t.integer("rate_limit_max"),
+	requestCount: t.integer("request_count"),
+	remaining: t.integer("remaining"),
+	lastRequest: t.timestamp("last_request", { withTimezone: true }),
+	expiresAt: t.timestamp("expires_at", { withTimezone: true }),
+	createdAt: t.timestamp("created_at", { withTimezone: true }).notNull(),
+	updatedAt: t.timestamp("updated_at", { withTimezone: true }).notNull(),
+	permissions: t.text("permissions"),
+	metadata: t.text("metadata"),
+});
+
 /**
  * APPLICATION-SPECIFIC TABLES
  */
